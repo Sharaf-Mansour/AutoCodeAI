5ce76b643baba674123d10270a3df06b83fdb4ca
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-06-27 06:51:37 +0300

    Waiting for the plugins to be added

---
+++ Services/CodePlugin.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+namespace AutoCodeAI.Services;
+
+public class CodePlugin
+{
+    [KernelFunction("save_code")]
+    [Description("Saves code to local file")]
+    public async Task SaveCodeAsync(string code,string fileName
+    )
+    {
+        Console.WriteLine("Did this shit work?");
+        Console.WriteLine(fileName);
+        Console.WriteLine(code);
+        await File.WriteAllTextAsync(fileName, code);
+    }
+    [KernelFunction("send_email")]
+    [Description("Sends email")]
+    public void SendMail()
+    {
+        Console.WriteLine("go fuck yourself");
+    }
+}
